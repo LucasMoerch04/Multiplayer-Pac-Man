566afb12081e55fa27aedcd93a989f880ac2a2b0
Lucas Metz Mørch <121830918+LucasMoerch04@users.noreply.github.com>
2025-03-20 18:20:29 +0100

    Delete dist directory

--- dist/__tests__/client/index.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-describe('index.html', () => {
-    it('should contain a canvas element with id "gameCanvas"', () => {
-        const canvas = document.querySelector('canvas#gameCanvas');
-        expect(canvas).toBeDefined();
-    });
-});
