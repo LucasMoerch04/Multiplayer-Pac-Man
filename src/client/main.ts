33dd53dc39c517fb6d03604a6097dc35eb8fcaec
Lucas Metz Mørch <121830918+LucasMoerch04@users.noreply.github.com>
2025-03-31 19:16:13 +0200

    Update main.ts
    added comments

--- src/client/main.ts
+++ src/client/main.ts
@@ -4,12 +4,14 @@ import './player';
 import './style.css';
 const socket = io(); // Connects to the server
 
+// On connection to socket, prints socket id and emits newUser function
 socket.on("connect", () => {
   console.log("Connected to server with id:", socket.id);
   socket.emit("newUser");
 });
 
+// After receiving updateCounter call, print the data given as argument
 socket.on("updateCounter", (data) => {
   console.log("Users conneceted:", data.countUsers);
-  (document.getElementById("userCounter") as HTMLElement).innerText = "Users Connected: " + data.countUsers;
+  (document.getElementById("userCounter") as HTMLElement).innerText = "Users Connected: " + data.countUsers; // change the innertext of the htmlElement with id of "userCounter" to show user count
 });
