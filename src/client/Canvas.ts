b1ad43c6adff373b79f4bbd67198055dc840645f
Lucas Moerch <lucasmoerch04@icloud.com>
2025-04-01 14:16:18 +0200

    move background image to game-assets folder

--- src/client/Canvas.ts
+++ src/client/Canvas.ts
@@ -2,7 +2,7 @@ const canvas: HTMLCanvasElement = document.getElementById("gameCanvas") as HTMLC
 const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
 //Jeg kan simpelhent ikke få det til at fungere at den henter baggrunden hvis baggrunden befinder sig i  game-assets
 const backgroundImage = new Image();
-backgroundImage.src = "/Background.png";
+backgroundImage.src = "game-assets/Background.png";
 console.log(backgroundImage)
 backgroundImage.onload = () => {
     ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
