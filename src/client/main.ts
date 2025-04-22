b19427bf39bac1ce9f8780a272d6f7e99b68aedd
Lucas Mørch <lucasmoerch04@icloud.com>
2025-04-22 15:12:22 +0200

    fix duplicate bug

--- src/client/main.ts
+++ src/client/main.ts
@@ -17,13 +17,13 @@ const socket = io(); // Connects to the server
 
 // On connection to socket, prints socket id and emits newUser function
 socket.on("connect", () => {
-  console.log("Connected to server with id:", socket.id);
+  console.log("Connected to server with id:", socket);
   socket.emit("newUser");
 });
 
 // After receiving updateCounter call, print the data given as argument
 socket.on("updateCounter", (data) => {
-  console.log("Users conneceted:", data.countUsers);
+  console.log("Users connected:", data.countUsers);
   (document.getElementById("userCounter") as HTMLElement).innerText =
     "Users Connected: " + data.countUsers; // change the innertext of the htmlElement with id of "userCounter" to show user count
 });
@@ -95,8 +95,6 @@ socket.on("updatePlayers", (backendPlayers) => {
   }
 
   animate(); // Call the animate function to draw the players on the canvas
-
-  //console.log(frontEndPlayers);
 });
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
