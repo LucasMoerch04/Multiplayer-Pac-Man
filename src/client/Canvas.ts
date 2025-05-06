37f89ecb8604a882338c0ea7acf61bc9d153c87d
Mads Scriver <madsulrichscriver@hotmail.com>
2025-05-06 13:42:19 +0200

    i fixed the issue of the speedboost only being deleted locally and i added a cherry power object, and gave it a sprite. i havent added any function to the cherries

--- src/client/Canvas.ts
+++ src/client/Canvas.ts
@@ -6,20 +6,32 @@ export const bgCtx: CanvasRenderingContext2D = bgCanvas.getContext("2d")!;
 
 // foreground canvas
 export const fgCanvas: HTMLCanvasElement = document.getElementById(
-  "gameState",) as HTMLCanvasElement;
+  "gameState",
+) as HTMLCanvasElement;
 export const fgCtx: CanvasRenderingContext2D = fgCanvas.getContext("2d")!;
 
+export const canvasHeight = bgCanvas.height;
+export const canvasWidth = bgCanvas.width;
 
 export const backgroundImage = new Image();
 backgroundImage.src = "game-assets/Background.png";
 backgroundImage.onload = () => {
-    bgCtx.drawImage(backgroundImage, 0, 0, bgCanvas.width, bgCanvas.height);
-    console.log(backgroundImage)
-  };
+  bgCtx.drawImage(backgroundImage, 0, 0, bgCanvas.width, bgCanvas.height);
+  console.log(backgroundImage);
+};
 
-export const redbull = new Image();
-redbull.src = "../game-assets/Redbull.png";
-console.log(redbull);
-redbull.onload = () => {
-    console.log(redbull)
-  };
+export const redbullImage = new Image();
+redbullImage.src = "../game-assets/Redbull.png";
+redbullImage.onload = () => {
+  console.log(redbullImage);
+};
+
+export const cherryImage = new Image();
+cherryImage.src = "../game-assets/Cherry.png";
+cherryImage.onload = () => {
+  console.log(cherryImage);
+};
+
+export function drawBackground() {
+  bgCtx.drawImage(backgroundImage, 0, 0, bgCanvas.width, bgCanvas.height);
+}
--- src/client/Powers.ts
+++ src/client/Powers.ts
@@ -1,83 +1,97 @@
-import { redbull } from "./Canvas";
+import { redbullImage, cherryImage } from "./Canvas";
 import { fgCtx } from "./Canvas";
+import { BaseEntity } from "../shared/entities";
 
+enum PowerType {
+  SPEED = "speed",
+  TELEPORT = "teleport",
+}
 
-class PowerObject {
-  public leftBorder: number = 0;
-  public rightBorder: number = 0;
-  public topBorder: number = 0;
-  public bottomBorder: number = 0;
-  public width: number = 32;
-  public height: number = 32;
-  public x: number;
-  public y: number;
-  public color: string;
+class PowerObject extends BaseEntity {
+  type: PowerType;
+  image?: HTMLImageElement;
 
-  constructor(x: number, y: number, color: string) {
-    this.x = x;
-    this.y = y;
-    this.color = color;
+  constructor(
+    x: number,
+    y: number,
+    type: PowerType,
+    image?: HTMLImageElement
+  ) {
+    super(x, y, "", 0); // Pass an empty string for color in BaseEntity
+    this.type = type;
+    this.image = image;
   }
 
-  drawObject() {
-    // Ensure the image is loaded before drawing
-    fgCtx.drawImage(redbull, this.x, this.y, this.width, this.height);
+  draw() {
+    if (this.image) {
+      fgCtx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    } else {
+      // Optionally, draw a placeholder or do nothing if no image is provided
+      fgCtx.fillStyle = "gray"; // Default placeholder color
+      fgCtx.fillRect(this.x, this.y, this.width, this.height);
+    }
   }
 
-  // Collision detection method
-  isColliding(
-    playerX: number,
-    playerY: number,
-    playerWidth: number,
-    playerHeight: number
-  ): boolean {
+  checkPlayerCollision(player: {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  }): boolean {
     return (
-      playerX < this.x + this.width &&
-      playerX + playerWidth > this.x &&
-      playerY < this.y + this.height &&
-      playerY + playerHeight > this.y
+      player.x < this.x + this.width &&
+      player.x + player.width > this.x &&
+      player.y < this.y + this.height &&
+      player.y + player.height > this.y
     );
   }
 }
 
+// Create new power items on the map
 export const speedObjects: PowerObject[] = [
-  new PowerObject(560, 620, "blue"),
-  new PowerObject(50, 1420, "blue"),
-  new PowerObject(1550, 370, "blue"),
-  // new PowerObject(canvas.width/2, canvas.width/2-100, "blue"),
+  new PowerObject(560, 620, PowerType.SPEED, redbullImage),
+  new PowerObject(50, 1420, PowerType.SPEED, redbullImage),
+  new PowerObject(1550, 370, PowerType.SPEED, redbullImage),
+];
+
+export const teleportObject: PowerObject[] = [
+  new PowerObject(290, 710, PowerType.TELEPORT),
+  new PowerObject(290 + 32, 710, PowerType.TELEPORT),
+  new PowerObject(928, 1350, PowerType.TELEPORT),
+  new PowerObject(928 + 32, 1350, PowerType.TELEPORT),
+];
+const firstBlock = 32;
+const lastBlock = 32*50;
+export const cherryObjects: PowerObject[] = [
+  new PowerObject(firstBlock, firstBlock, PowerType.SPEED, cherryImage),
+  new PowerObject(lastBlock, firstBlock, PowerType.SPEED, cherryImage),
+  new PowerObject(firstBlock, lastBlock, PowerType.SPEED, cherryImage),
+  new PowerObject(lastBlock, lastBlock, PowerType.SPEED, cherryImage),
 ];
 
 // This function returns the index of the object that the player is colliding with
-export function SpeedObjectCollision(
-  playerX: number,
-  playerY: number,
-  playerWidth: number,
-  playerHeight: number
-): number | null {
+export function SpeedObjectCollision(player: {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}): number {
   for (let i = 0; i < speedObjects.length; i++) {
-    if (speedObjects[i].isColliding(playerX, playerY, playerWidth, playerHeight)) {
-      speedObjects.splice(i, 1);
+    if (speedObjects[i].checkPlayerCollision(player)) {
       return i;
     }
   }
-  return -1; // If nothing is hit, return null
+  return -1; // If nothing is hit, return -1
 }
 
-
-export const teleportObject: PowerObject[] = [
-  new PowerObject(290, 710, "blue"),
-  new PowerObject(290+32, 710, "blue"),
-  new PowerObject(928, 1350, "blue"),
-  new PowerObject(928+32, 1350, "blue"),
-];
-export function teleportObjectObjectCollision(
-  playerX: number,
-  playerY: number,
-  playerWidth: number,
-  playerHeight: number
-): number | null {
+export function teleportObjectCollision(player: {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}): number {
   for (let i = 0; i < teleportObject.length; i++) {
-    if (teleportObject[i].isColliding(playerX, playerY, playerWidth, playerHeight)) {
+    if (teleportObject[i].checkPlayerCollision(player)) {
       console.log(`Colliding with teleport object at index ${i}:`, i);
       return i;
     }
@@ -85,30 +99,16 @@ export function teleportObjectObjectCollision(
   return -1;
 }
 
-
-export const pacmanCherry: PowerObject[] = [
-  new PowerObject(80, 80, "red"),
-  new PowerObject(200, 200, "red"),
-  new PowerObject(300, 300, "red"),
-
-];
-export function pacmanCherryCollision(
-  playerX: number,
-  playerY: number,
-  playerWidth: number,
-  playerHeight: number
-): number | null {
-  for (let i = 0; i < pacmanCherry.length; i++) {
-    if (pacmanCherry[i].isColliding(playerX, playerY, playerWidth, playerHeight)) {
-      console.log ("pacman colliding")
-      pacmanCherry.splice(i, 1);
+export function cherryObjectCollision(player: {
+  x: number;
+  y: number;
+  width: number;
+  height: number
+}): number {
+  for (let i = 0; i < cherryObjects.length; i++) {
+    if (cherryObjects[i].checkPlayerCollision(player)) {
       return i;
     }
   }
-  return -1;
+  return -1; // If nothing is hit, return -1
 }
-
-
-
-
-
